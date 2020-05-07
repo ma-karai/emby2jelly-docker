10ff928c9ee74cda0c140d20168950a739ed214f
makarai <17119942+ma-karai@users.noreply.github.com>
2020-05-07 08:55:09 +0200

    Update rewritesettings.sh

--- rewritesettings.sh
+++ rewritesettings.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 echo "execute rewrite settings and begin watchstatus migration"
 
+rm $EMBY2JELLY_HOME/settings.ini
 
-
+printf  "[Emby]\n" >> $EMBY2JELLY_HOME/settings.ini
+printf  "EMBY_APIKEY = $EMBY_APIKEY\n" >> $EMBY2JELLY_HOME/settings.ini
+printf  "EMBY_URLBASE = $EMBY_URLBASE\n" >> $EMBY2JELLY_HOME/settings.ini
+printf  "[Jelly] \n" >> $EMBY2JELLY_HOME/settings.ini
+printf  "JELLY_APIKEY = $JELLY_APIKEY\n" >> $EMBY2JELLY_HOME/settings.ini
+printf  "JELLY_URLBASE = $JELLY_URLBASE\n" >> $EMBY2JELLY_HOME/settings.ini
 
 
 

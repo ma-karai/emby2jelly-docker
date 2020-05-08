f91d2e7a125dc720df3044620f3113cffb7aa2e1
makarai <17119942+ma-karai@users.noreply.github.com>
2020-05-08 10:01:25 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,6 +12,7 @@ printf  "JELLY_URLBASE = $JELLY_URLBASE\n" >> $EMBY2JELLY_HOME/settings.ini
 
 echo " begin watchstatus migration"
 
-/usr/bin/nohup python $EMBY2JELLY_HOME/APImain.py --newuserpwd $NEW_USER_PWD 
+/usr/bin/nohup python $EMBY2JELLY_HOME/APImain.py 
+#--newuserpwd $NEW_USER_PWD 
 sleep 0.5
-tee "$EMBY2JELLY_HOME/nohup.out > /proc/1/fd/1 2>/proc/1/fd/2
+tee "/root/.config/Emby2Jelly/nohup.out > /proc/1/fd/1 2>/proc/1/fd/2

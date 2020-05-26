0c432a9d60d67406ce866c6cb381ea4ae83ad9a4
makarai <17119942+ma-karai@users.noreply.github.com>
2020-05-26 14:10:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ printf  "[Jelly] \n" >> $EMBY2JELLY_HOME/settings.ini
 printf  "JELLY_APIKEY = $JELLY_APIKEY\n" >> $EMBY2JELLY_HOME/settings.ini
 printf  "JELLY_URLBASE = $JELLY_URLBASE\n" >> $EMBY2JELLY_HOME/settings.ini
 
-echo "migration should be starting"
+echo "migration should start"
 
 /usr/bin/nohup python $EMBY2JELLY_HOME/APImain.py --new-user-pw $NEW_USER_PWD  
 

bc31f70eef53f531d64e5249d365523cba0d6f88
makarai <17119942+ma-karai@users.noreply.github.com>
2020-05-26 14:50:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,8 @@ printf  "[Jelly] \n" >> $EMBY2JELLY_HOME/settings.ini
 printf  "JELLY_APIKEY = $JELLY_APIKEY\n" >> $EMBY2JELLY_HOME/settings.ini
 printf  "JELLY_URLBASE = $JELLY_URLBASE\n" >> $EMBY2JELLY_HOME/settings.ini
 
-echo "start process manually with "
+echo "start process manually with"
+echo "docker exec -it emby-to-jelly python3 $EMBY2JELLY_HOME/APImain.py --new-user-pw $NEW_USER_PWD "
 
 #/usr/bin/nohup python $EMBY2JELLY_HOME/APImain.py --new-user-pw $NEW_USER_PWD  
 

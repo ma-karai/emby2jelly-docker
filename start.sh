5a026f416e44e35767f83ca354c6eeafcd20344d
makarai <17119942+ma-karai@users.noreply.github.com>
2020-05-26 15:36:04 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -23,7 +23,7 @@ echo "start process manually with"
 echo "docker exec -it emby-to-jelly python3 $EMBY2JELLY_HOME/APImain.py --new-user-pw $NEW_USER_PWD "
 echo "-----------------------------"
 
-#/usr/bin/nohup -u python $EMBY2JELLY_HOME/APImain.py --new-user-pw $NEW_USER_PWD > /root/.config/Emby2Jelly/nohup.out &
+/usr/bin/nohup -u python3 $EMBY2JELLY_HOME/APImain.py --new-user-pw $NEW_USER_PWD > /root/.config/Emby2Jelly/nohup.out &
 
 #echo "end of process"
 
